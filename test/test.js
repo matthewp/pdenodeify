fbf781017600682436142753515ff3a58f2e9ea7
Matthew Phillips <matthew@matthewphillips.info>
2014-10-07 07:58:01 -0400

    Adds test for failing scenario

--- test/test.js
+++ test/test.js
@@ -10,7 +10,16 @@ describe('Basics', function() {
       var data = JSON.parse(txt);
       assert.equal(data.name, 'pdenodeify');
     }).then(done);
+  });
 
+  it('handles errors correctly', function(done) {
+    var readFile = denodeify(require('fs').readFile);
+
+    readFile(__dirname + '/fakefile.txt').then(function() {
+      assert.fail(true, false, 'Should not have gotten here');
+    }, function(err) {
+      assert(err instanceof Error, 'An error received')
+    }).then(done);
   });
 
 });
